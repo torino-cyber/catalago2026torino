4c008d3bc4bfa6db40cf5635588b998acc03f3fe
torino-cyber <torinomodasbr@gmail.com>
2026-02-01 13:00:50 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+const produtos = [
+  {
+    id: 1,
+    nome: "Jaqueta Feminina PV",
+    categoria: "Feminino",
+    estacao: "Primavera/Verão",
+    precoGrade: 45,
+    precoCaixa: 38,
+    minGrade: 24,
+    qtdCaixa: 200
+  },
+  {
+    id: 2,
+    nome: "Conjunto Infantil Inverno",
+    categoria: "Infantil",
+    estacao: "Outono/Inverno",
+    precoGrade: 39,
+    precoCaixa: 32,
+    minGrade: 24,
+    qtdCaixa: 120
+  }
+];
